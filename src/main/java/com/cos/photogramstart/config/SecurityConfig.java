c9d9564cb5218f762e8626d4d433cab1f1add376
sunny <cws2000kr@naver.com>
2022-04-07 18:26:13 +0900

    SecurityConfig 설정, CSRF 토큰, 회원가입 post

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // security 활성화
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		//super.configure(http);
+		
+		http.csrf().disable(); // csrf 비활성화
+		
+		http.authorizeRequests()
+		.antMatchers("/", "/user/**", "/image/***", "/subscribe/**", "/comment/**").authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/auth/signin")
+		.defaultSuccessUrl("/");
+	}
+}
