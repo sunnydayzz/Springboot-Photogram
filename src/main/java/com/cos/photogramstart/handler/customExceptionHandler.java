438d6b37d0478a686a0ef9f8b3a6d0f9b1e561fc
sunny <cws2000kr@naver.com>
2022-04-15 10:24:09 +0900

    회원가입 : 공통응답 Script 만들기

--- src/main/java/com/cos/photogramstart/handler/customExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/customExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
+import com.cos.photogramstart.util.Script;
 import com.cos.photogramstart.web.dto.CMRespDto;
 
 @RestController
@@ -16,9 +17,16 @@ public class customExceptionHandler {
 
 	private Map<String, String> errorMap = new HashMap<>();
 	
+	// Ajax, Android 통신용
+//	@ExceptionHandler(CustomValidationException.class)
+//	public CMRespDto<?> customValidationException(CustomValidationException e) {
+//		
+//		return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap());
+//	}
+	
+	// 브라우저 응답용
 	@ExceptionHandler(CustomValidationException.class)
-	public CMRespDto<?> customValidationException(CustomValidationException e) {
-		
-		return new CMRespDto<>(-1, e.getMessage(), e.getErrorMap());
+	public String cutomValidationException(CustomValidationException e) {
+		return Script.back(e.getErrorMap().toString());
 	}
 }
