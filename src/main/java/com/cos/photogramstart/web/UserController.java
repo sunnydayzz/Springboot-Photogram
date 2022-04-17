1de0a4be76126416d698fc132df26788db77a92a
sunny <cws2000kr@naver.com>
2022-04-17 14:23:21 +0900

    로그인 : View 연결하기

---
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -0,0 +1,15 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class UserController {
+
+	@GetMapping("/user/{id}")
+	public String popular(@PathVariable int id) {
+		
+		return "user/profile";
+	}
+}
